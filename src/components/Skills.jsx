32671c42582ad6f20ed06a702dad08d578dfbe48
bunn1 <nicjohbun@gmail.com>
2024-03-08 21:47:48 +0100

    Creating components

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -12,12 +12,12 @@ import Tailwind from '../assets/tailwind.png';
 
 function Skill() {
   return (
-    <div name="skill" className="bg-[#0a192f] text-gray-300">
+    <div name="skill" className=" w-full h-screen bg-[#0a192f] text-gray-300">
       {/*Container*/}
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div>
           <p className="text-4xl font-bold inline border-b-4 border-pink-600">
-            Erfarenhet
+            Färdigheter
           </p>
           <p className="py-4">Detta är teknologierna jag jobbat med.</p>
         </div>
