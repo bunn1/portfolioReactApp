82afe7df28b0f089d8b4581585ec8469a77f256d
bunn1 <nicjohbun@gmail.com>
2024-03-10 15:46:11 +0100

    naming components Link and import react-scroll Creating contact component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Logo from '../assets/logo2.png';
+import { Link } from 'react-scroll';
 
 import {
   FaBars,
@@ -24,11 +25,31 @@ function Navbar() {
       </div>
       {/* menu */}
       <ul className="hidden md:flex">
-        <li>Home</li>
-        <li>About</li>
-        <li>Skills</li>
-        <li>Work</li>
-        <li>Contact</li>
+        <li>
+          <Link activeClass="active" to="home" smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link activeClass="active" to="about" smooth={true} duration={500}>
+            About
+          </Link>
+        </li>
+        <li>
+          <Link activeClass="active" to="skills" smooth={true} duration={500}>
+            Skills
+          </Link>
+        </li>
+        <li>
+          <Link activeClass="active" to="work" smooth={true} duration={500}>
+            Work
+          </Link>
+        </li>
+        <li>
+          <Link activeClass="active" to="contact" smooth={true} duration={500}>
+            Contact
+          </Link>
+        </li>
       </ul>
 
       {/*Hamburger*/}
@@ -36,7 +57,7 @@ function Navbar() {
         {!nav ? <FaBars /> : <FaTimes />}
       </div>
 
-      {/*Mobile Menu*/}
+      {/* Mobile menu */}
       <ul
         className={
           !nav
@@ -44,11 +65,35 @@ function Navbar() {
             : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'
         }
       >
-        <li className="py-6 text-4xl">Home</li>
-        <li className="py-6 text-4xl">About</li>
-        <li className="py-6 text-4xl">Skills</li>
-        <li className="py-6 text-4xl">Work</li>
-        <li className="py-6 text-4xl">Contact</li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="home" smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          {' '}
+          <Link onClick={handleClick} to="about" smooth={true} duration={500}>
+            About
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          {' '}
+          <Link onClick={handleClick} to="skills" smooth={true} duration={500}>
+            Skills
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          {' '}
+          <Link onClick={handleClick} to="work" smooth={true} duration={500}>
+            Work
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          {' '}
+          <Link onClick={handleClick} to="contact" smooth={true} duration={500}>
+            Contact
+          </Link>
+        </li>
       </ul>
 
       {/*Social Icons*/}
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -12,7 +12,7 @@ import Tailwind from '../assets/tailwind.png';
 
 function Skill() {
   return (
-    <div name="skill" className=" w-full h-screen bg-[#0a192f] text-gray-300">
+    <div name="skills" className=" w-full h-screen bg-[#0a192f] text-gray-300">
       {/*Container*/}
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div>
