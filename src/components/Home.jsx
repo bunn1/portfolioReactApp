6c2fd35cf290b60eb27b7514f822989a450afb1d
bunn1 <nicjohbun@gmail.com>
2024-04-13 14:17:52 +0200

    text adding

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { HiArrowNarrowRight } from 'react-icons/hi';
 
 const Home = () => {
@@ -6,17 +7,18 @@ const Home = () => {
     <div name="home" className="w-full h-screen bg-[#0a192f]">
       {/* Container */}
       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
-        <p className="text-yellow-400">Hej, mitt namn är Niclas Bunn</p>
-        <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
+        <p className="text-yellow-400 my-2 sm:text-2xl">Hi, my name is:</p>
+        <h1 className="text-4xl sm:text-6xl font-bold text-[#ccd6f6]">
           Niclas Bunn
         </h1>
-        <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
-          Jag är en web utvecklare.
+        <h2 className="text-4xl sm:text-5xl font-bold my-2 text-[#8892b0]">
+          I´m a web developer.
         </h2>
         <p className="text-[#8892b0] py-4 max-w-[700px]">
-          Jag är en Web utvecklar som är specialiserad att göra responsive
-          hemsidor. För närvarande arbetar jag med tailwind och react.
+          I'm a web developer who currently is working with Javascript, Tailwind
+          and React.
         </p>
+
         <div>
           <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-yellow-400 hover:border-yellow-400">
             View Work
