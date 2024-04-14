4a96b99e32bf9d63de42bf143346b88e32d711c3
bunn1 <nicjohbun@gmail.com>
2024-04-14 08:18:41 +0200

    id-work text insert

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
+import toDo from '../assets/toDo.jpg';
+
 // import WorkImg from '../assets/workImg.jpeg';
 // import realEstate from '../assets/realestate.jpg';
 
 function Work() {
   return (
     <div
+      id="work"
       name="work"
       className="w-full md:h-screen text-gray-300  bg-[#0a192f]
     "
@@ -13,9 +16,9 @@ function Work() {
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full ">
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-yellow-400">
-            Arbeten
+            Work
           </p>
-          <p className="py-6">Kolla in några av mina arbeten</p>
+          <p className="py-6">Check my work!</p>
         </div>
 
         {/* Container */}
@@ -23,7 +26,7 @@ function Work() {
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
           {/* Grid Item */}
           <div
-            //   style={{ backgroundImage: `url(${WorkImg})` }}
+            style={{ backgroundImage: `url(${toDo})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
             {/* Hover Effects */}
@@ -46,7 +49,7 @@ function Work() {
             </div>
           </div>
           <div
-            //   style={{ backgroundImage: `url(${WorkImg})` }}
+            // style={{ backgroundImage: `url(${WorkImg})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
             {/* Hover Effects */}
@@ -70,7 +73,7 @@ function Work() {
           </div>
           {/* Grid Item */}
           <div
-            //   style={{ backgroundImage: `url(${WorkImg})` }}
+            style={{ backgroundImage: `url(${toDo})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
             {/* Hover Effects */}
