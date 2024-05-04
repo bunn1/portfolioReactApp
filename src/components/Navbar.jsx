a7160491966b303c8dea5d00c1213f0de1c2cce4
bunn1 <nicjohbun@gmail.com>
2024-05-04 11:01:00 +0200

    Outcomment mail and resume icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -115,6 +115,7 @@ function Navbar() {
               Github <FaGithub size={30} />
             </a>
           </li>
+          {/* 
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]">
             <a
               className="flex justify-between items-center w-full text-gray-300"
@@ -123,6 +124,7 @@ function Navbar() {
               Email <HiOutlineMail size={30} />
             </a>
           </li>
+        
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
             <a
               className="flex justify-between items-center w-full text-gray-300"
@@ -131,6 +133,7 @@ function Navbar() {
               Resume <BsFillPersonLinesFill size={30} />
             </a>
           </li>
+          */}
         </ul>
       </div>
     </div>
