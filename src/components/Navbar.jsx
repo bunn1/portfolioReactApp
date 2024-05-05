5ebbf853a2afc629798e597b6cfc071b39aaba81
bunn1 <nicjohbun@gmail.com>
2024-05-05 21:04:16 +0200

    Out comment link to linkedIn, Github and mail

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -100,6 +100,7 @@ function Navbar() {
       {/*Social Icons*/}
       <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
         <ul>
+          {/* 
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600">
             <a
               className="flex justify-between items-center w-full text-gray-300"
@@ -116,7 +117,7 @@ function Navbar() {
               Github <FaGithub size={30} />
             </a>
           </li>
-          {/* 
+          
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]">
             <a
               className="flex justify-between items-center w-full text-gray-300"
